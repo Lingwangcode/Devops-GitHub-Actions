5d3f887c5910862ba45740d4cc4cff4eb5f8d44e
Ling Wang <amyling.wang@gmail.com>
2023-10-02 14:16:25 +0200

    Initial commit

---
+++ src/test/java/com/example/devopsgithubactions/DevopsGitHubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopsgithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsGitHubActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
